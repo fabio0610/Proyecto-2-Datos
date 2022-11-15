09bac407ead176e43df32d81cfb09911a5ed9d7a
Naz González ✨ <99229145+NazGonzalez@users.noreply.github.com>
2022-11-14 22:57:58 -0600

    Prueba

--- ProyectoGato/main.py
+++ ProyectoGato/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Proyecto de Datos')
+    print_hi('Proyecto')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
