119d8d75a5455fbde9d470b90f066e027945355a
fabio0610 <fabio0610@hotmail.com>
2022-11-15 16:23:59 -0600

    Inicio de implementacion del menu

--- ProyectoGato/main.py
+++ ProyectoGato/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("Bienvenido al juego de gato")
+print("\n\n\nElija una de las siguientes opciones: ")
+print("\n")
+print("1) Jugador vs Jugador")
+print("2) Jugador vs CPU")
+print("3) Salir")
 
+opcion = input("Ingrese una opcion: ")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Proyecto')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
