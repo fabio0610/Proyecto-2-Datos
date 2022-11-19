51b8928eccd4fddcf236950362ebbb7390c56f03
fabio0610 <fabio0610@hotmail.com>
2022-11-19 15:00:25 -0600

    se termina el menu

--- ProyectoGato/main.py
+++ ProyectoGato/main.py
@@ -2,9 +2,50 @@
 print("Bienvenido al juego de gato")
 print("\n\n\nElija una de las siguientes opciones: ")
 print("\n")
-print("1) Jugador vs Jugador")
-print("2) Jugador vs CPU")
-print("3) Salir")
 
-opcion = input("Ingrese una opcion: ")
+def menu_principal():
+    print("1) Jugador vs Jugador")
+    print("2) Jugador vs CPU")
+    print("3) Salir")
+
+def menu_CPU():
+    print("Elija una dificultad: ")
+    print("1) Facil")
+    print("2) Normal")
+    print("3) Dificil")
+    print("4) Volver")
+
+
+
+#valido = False
+
+while (True):
+    print("\n\n")
+    menu_principal()
+    opcion = input("Ingrese una opcion: ")
+    if (opcion == "1"):
+        print(" ")
+    elif (opcion == "2"):
+        print("\n\n")
+        while (True):
+            menu_CPU()
+            opcion = input("Ingrese una opcion: ")
+            if(opcion == "1"):
+                print(" ")
+            elif (opcion == "2"):
+                print(" ")
+            elif (opcion == "3"):
+                print(" ")
+            elif (opcion == "4"):
+                break
+            else:
+                print("Opcion incorrecta, imgrese una opcion valida ")
+                print("\n\n")
+
+    elif (opcion == "3"):
+        break
+    else:
+        #valido = True
+        print("Opcion incorrecta, imgrese una opcion valida ")
+        print("\n\n")
 
